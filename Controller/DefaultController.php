b18890cefd4af9d3f3b1374b05c124f74dabbf49
Karol Mocniak <karol@mocniak.pl>
2021-01-12 19:15:54 +0100

    psr4 rename

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mocniak\PropagandaCMS\Infrastructure\Controller;
+namespace Mocniak\PropagandaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
